7cace4e4a433688a6a112e187904b34b4801b1e2
PeytonChester <peytonchester1@gmail.com>
2019-06-17 16:30:04 -0400

    Initial commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Peyton Chester on 6/13/19.
+//  Copyright © 2019 Peyton Chester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
